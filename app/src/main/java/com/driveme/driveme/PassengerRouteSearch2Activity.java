e5e2669c6d7538b8b24663f884fae01877f266b3
Shayan Malinda <shayanmalinda@gmail.com>
2019-08-27 00:48:51 +0530

    route search and view checkpoints

--- app/src/main/java/com/driveme/driveme/PassengerRouteSearch2Activity.java
+++ app/src/main/java/com/driveme/driveme/PassengerRouteSearch2Activity.java
@@ -10,14 +10,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.internal.et;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -32,6 +39,18 @@ public class PassengerRouteSearch2Activity extends AppCompatActivity {
     private static ArrayList<String> filteredRoutes = new ArrayList<String>();
 
     FirebaseFirestore db;
+    String driverId;
+    String allowedPassengers;
+    String currentPassengers;
+    String startPlace;
+    String endPlace;
+    String startTime;
+    String endTime;
+    String driverEmail;
+    String driverTelephone;
+    String vehicleType;
+    String isAC;
+    String vehicleNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +66,7 @@ public class PassengerRouteSearch2Activity extends AppCompatActivity {
         dialog.show();
 
         db = FirebaseFirestore.getInstance();
+        final ListView lv = findViewById(R.id.route_list);
 
         filteredRoutes = extras.getStringArrayList("filteredRoutes");
 
@@ -64,17 +84,32 @@ public class PassengerRouteSearch2Activity extends AppCompatActivity {
                 public void onSuccess(DocumentSnapshot documentSnapshot) {
                     HashMap<String,String> map  = new HashMap();
                     map.put("driverId",documentSnapshot.getId());
-                    map.put("startPlace",documentSnapshot.getString("startPlaceName"));
-                    map.put("endPlace",documentSnapshot.getString("endPlaceName"));
-                    map.put("startTime",documentSnapshot.getString("startTime"));
-                    map.put("endTime",documentSnapshot.getString("endTime"));
-                    list.add(map);
+                    map.put("allowedPassengers: ",String.valueOf(documentSnapshot.getDouble("allowedPassengers")));
+                    map.put("currentPassengers: ",String.valueOf(documentSnapshot.getDouble("currentPassengers")));
+                    map.put("startPlace","Start Place  :  "+documentSnapshot.getString("startPlaceName"));
+                    map.put("endPlace","End Place  :  "+documentSnapshot.getString("endPlaceName"));
+                    map.put("startTime","Start Time  :  "+documentSnapshot.getString("startTime"));
+                    map.put("endTime","End Time  :  "+documentSnapshot.getString("endTime"));
+                    map.put("driverEmail","Driver Email  :  "+documentSnapshot.getString("email"));
+                    map.put("driverTelephone","Driver Telephone  :  "+documentSnapshot.getString("driverTelephone"));
+                    map.put("vehicleType","Vehicle Type  :  "+documentSnapshot.getString("vehicleType"));
+                    if(documentSnapshot.getBoolean("isAC")){
+                        map.put("isAC","Air Condition  :  "+"Yes");
+                    }
+                    else{
+                        map.put("isAC","Air Condition  :  "+"No");
+                    }
+                    map.put("vehicleNumber","Vehicle Number  :  "+documentSnapshot.getString("vehicleNumber"));
+                    if(documentSnapshot.getDouble("allowedPassengers")>documentSnapshot.getDouble("currentPassengers")){
+                        list.add(map);
+                    }
 
-                    final ListView lv = findViewById(R.id.route_list);
 
                     int layout = R.layout.item_route;
-                    String[] cols = {"driverId","startPlace","startTime","endPlace","endTime"};
-                    int[] views = {R.id.driverId,R.id.startPlace,R.id.startTime,R.id.endPlace,R.id.endTime};
+                    String[] cols = {"driverId","allowedPassengers","currentPassengers","startPlace","startTime","endPlace","endTime",
+                            "driverEmail","driverTelephone","vehicleType","isAC","vehicleNumber"};
+                    int[] views = {R.id.driverId,R.id.allowedPassengers,R.id.currentPassengers,R.id.startPlace,R.id.startTime,R.id.endPlace,
+                            R.id.endTime,R.id.driverEmail,R.id.driverTelephone,R.id.vehicleType,R.id.isAC,R.id.vehicleNumber};
                     SimpleAdapter adapter = new SimpleAdapter(PassengerRouteSearch2Activity.this,list,layout,cols,views);
                     lv.setAdapter(adapter);
                     dialog.dismiss();
@@ -93,6 +128,51 @@ public class PassengerRouteSearch2Activity extends AppCompatActivity {
         }
 
 
+
+    }
+
+    public void viewdetails(View v){
+        LinearLayout view = (LinearLayout)v.getParent();
+
+        TextView etDriverId = view.findViewById(R.id.driverId);
+        driverId = etDriverId.getText().toString();
+        TextView etAllowedPassengers = view.findViewById(R.id.allowedPassengers);
+        allowedPassengers = etAllowedPassengers.getText().toString();
+        TextView etCurrentPassengers = view.findViewById(R.id.currentPassengers);
+        currentPassengers = etCurrentPassengers.getText().toString();
+        TextView etStartPlace = view.findViewById(R.id.startPlace);
+        startPlace = etStartPlace.getText().toString();
+        TextView etStartTime = view.findViewById(R.id.startTime);
+        startTime = etStartTime.getText().toString();
+        TextView etEndPlace = view.findViewById(R.id.endPlace);
+        endPlace = etEndPlace.getText().toString();
+        TextView etEndTime = view.findViewById(R.id.endTime);
+        endTime = etEndTime.getText().toString();
+        TextView etDriverEmail = view.findViewById(R.id.driverEmail);
+        driverEmail = etDriverEmail.getText().toString();
+        TextView etDriverTelephone = view.findViewById(R.id.driverTelephone);
+        driverTelephone = etDriverTelephone.getText().toString();
+        TextView etvehicleType = view.findViewById(R.id.vehicleType);
+        vehicleType = etvehicleType.getText().toString();
+        TextView etisAC = view.findViewById(R.id.isAC);
+        isAC = etisAC.getText().toString();
+        TextView etvehicleNumber = view.findViewById(R.id.vehicleNumber);
+        vehicleNumber = etvehicleNumber.getText().toString();
+
+        Intent intent = new Intent(PassengerRouteSearch2Activity.this,PassengerRouteSearch3Activity.class);
+        intent.putExtra("driverId",driverId);
+        intent.putExtra("allowedPassengers",allowedPassengers);
+        intent.putExtra("currentPassengers",currentPassengers);
+        intent.putExtra("startPlace",startPlace);
+        intent.putExtra("startTime",startTime);
+        intent.putExtra("endPlace",endPlace);
+        intent.putExtra("endTime",endTime);
+        intent.putExtra("driverEmail",driverEmail);
+        intent.putExtra("driverTelephone",driverTelephone);
+        intent.putExtra("vehicleType",vehicleType);
+        intent.putExtra("isAC",isAC);
+        intent.putExtra("vehicleNumber",vehicleNumber);
+        startActivity(intent);
     }
 
 }
