3114c2ab2aeb58525b96b5325634df9c7d9eaab7
Shayan Malinda <shayanmalinda@gmail.com>
2019-09-03 20:57:46 +0530

    Animations on Card View

--- app/src/main/java/com/driveme/driveme/DriverHomeActivity.java
+++ app/src/main/java/com/driveme/driveme/DriverHomeActivity.java
@@ -6,8 +6,12 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -20,6 +24,18 @@ public class DriverHomeActivity extends AppCompatActivity {
     private CardView driverroute;
     private CardView livelocation;
     private CardView passengerlist;
+    private CardView ratepassengers;
+    private CardView payments;
+    private CardView myprofile;
+
+
+    private ImageView imgroute;
+    private ImageView imgsharelocation;
+    private ImageView imgpassengerlist;
+    private ImageView imgratepassengers;
+    private ImageView imgpayments;
+    private ImageView imgmyprofile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +43,42 @@ public class DriverHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_driver_home);
         setTitle("Driver - Home");
 
+
         driverroute = findViewById(R.id.driverroute);
         livelocation = findViewById(R.id.livelocation);
         passengerlist = findViewById(R.id.passengerlist);
+        ratepassengers = findViewById(R.id.ratepassengers);
+        payments = findViewById(R.id.payments);
+        myprofile = findViewById(R.id.myprofile);
+
+        imgroute = findViewById(R.id.imgroute);
+        imgsharelocation = findViewById(R.id.imgsharelocation);
+        imgpassengerlist = findViewById(R.id.imgpassengerlist);
+        imgratepassengers = findViewById(R.id.imgratepassenger);
+        imgpayments = findViewById(R.id.imgpayments);
+        imgmyprofile = findViewById(R.id.imgmyprofile);
+
+        final Animation animation = AnimationUtils.loadAnimation(DriverHomeActivity.this,R.anim.rotate);
 
         driverroute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(DriverHomeActivity.this,DriverRouteActivity.class);
-                startActivity(intent);
+                imgroute.startAnimation(animation);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        Intent intent = new Intent(DriverHomeActivity.this,DriverRouteActivity.class);
+                        startActivity(intent);
+                    }
+                }, 0);
+
             }
         });
 
         livelocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                imgsharelocation.startAnimation(animation);
                 Intent intent = new Intent(DriverHomeActivity.this,DriverMapActivity.class);
                 startActivity(intent);
             }
@@ -50,11 +87,54 @@ public class DriverHomeActivity extends AppCompatActivity {
         passengerlist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                imgpassengerlist.startAnimation(animation);
                 Intent intent = new Intent(DriverHomeActivity.this,DriverPassengerListActivity.class);
                 startActivity(intent);
             }
         });
 
+        ratepassengers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgratepassengers.startAnimation(animation);
+
+            }
+        });
+
+        payments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgpayments.startAnimation(animation);
+
+            }
+        });
+
+        myprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgmyprofile.startAnimation(animation);
+
+            }
+        });
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        imgroute = findViewById(R.id.imgroute);
+        imgsharelocation = findViewById(R.id.imgsharelocation);
+        imgpassengerlist = findViewById(R.id.imgpassengerlist);
+        imgratepassengers = findViewById(R.id.imgratepassenger);
+        imgpayments = findViewById(R.id.imgpayments);
+        imgmyprofile = findViewById(R.id.imgmyprofile);
+
+        imgroute.clearAnimation();
+        imgsharelocation.clearAnimation();
+        imgpassengerlist.clearAnimation();
+        imgratepassengers.clearAnimation();
+        imgpayments.clearAnimation();
+        imgmyprofile.clearAnimation();
+    }
 }
--- app/src/main/java/com/driveme/driveme/PassengerHomeActivity.java
+++ app/src/main/java/com/driveme/driveme/PassengerHomeActivity.java
@@ -8,6 +8,9 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -21,6 +24,15 @@ public class PassengerHomeActivity extends AppCompatActivity {
     private CardView routesearch;
     private CardView driverlocation;
     private CardView myroute;
+    private CardView ratedriver;
+    private CardView payment;
+
+    private ImageView imgroutesearch;
+    private ImageView imgmyprofile;
+    private ImageView imgdriverlocation;
+    private ImageView imgmyroute;
+    private ImageView imgratedriver;
+    private ImageView imgpayment;
 
 
 
@@ -35,10 +47,22 @@ public class PassengerHomeActivity extends AppCompatActivity {
         routesearch = findViewById(R.id.searchroute);
         driverlocation = findViewById(R.id.driverlocation);
         myroute = findViewById(R.id.myroute);
+        ratedriver = findViewById(R.id.ratedriver);
+        payment = findViewById(R.id.payments);
+
+        imgmyprofile = findViewById(R.id.imgmyprofile);
+        imgroutesearch = findViewById(R.id.imgsearch);
+        imgdriverlocation = findViewById(R.id.imgdriverlocation);
+        imgmyroute = findViewById(R.id.imgmyroute);
+        imgratedriver = findViewById(R.id.imgratedriver);
+        imgpayment = findViewById(R.id.imgpayment);
+
+        final Animation animation = AnimationUtils.loadAnimation(PassengerHomeActivity.this,R.anim.rotate);
 
         myprofile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                imgmyprofile.startAnimation(animation);
                 Intent intent = new Intent(PassengerHomeActivity.this,PassengerProfileActivity.class);
                 startActivity(intent);
             }
@@ -47,7 +71,7 @@ public class PassengerHomeActivity extends AppCompatActivity {
         routesearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                imgroutesearch.startAnimation(animation);
                 Intent intent = new Intent(PassengerHomeActivity.this,PassengerRouteSearchActivity.class);
                 startActivity(intent);
 
@@ -60,6 +84,7 @@ public class PassengerHomeActivity extends AppCompatActivity {
         driverlocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                imgdriverlocation.startAnimation(animation);
                 Intent intent = new Intent(PassengerHomeActivity.this,PassengerMapActivity.class);
                 startActivity(intent);
             }
@@ -68,7 +93,7 @@ public class PassengerHomeActivity extends AppCompatActivity {
         myroute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                imgmyroute.startAnimation(animation);
                 AlertDialog.Builder builder = new AlertDialog.Builder(PassengerHomeActivity.this);
                 builder.setCancelable(false); // if you want user to wait for some process to finish,
                 builder.setView(R.layout.layout_loading_dialog);
@@ -100,5 +125,40 @@ public class PassengerHomeActivity extends AppCompatActivity {
 
             }
         });
+
+        ratedriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgratedriver.startAnimation(animation);
+
+            }
+        });
+
+        payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgpayment.startAnimation(animation);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        imgmyprofile = findViewById(R.id.imgmyprofile);
+        imgroutesearch = findViewById(R.id.imgsearch);
+        imgdriverlocation = findViewById(R.id.imgdriverlocation);
+        imgmyroute = findViewById(R.id.imgmyroute);
+        imgratedriver = findViewById(R.id.imgratedriver);
+        imgpayment = findViewById(R.id.imgpayment);
+
+        imgmyroute.clearAnimation();
+        imgmyprofile.clearAnimation();
+        imgdriverlocation.clearAnimation();
+        imgratedriver.clearAnimation();
+        imgpayment.clearAnimation();
+        imgroutesearch.clearAnimation();
     }
 }
