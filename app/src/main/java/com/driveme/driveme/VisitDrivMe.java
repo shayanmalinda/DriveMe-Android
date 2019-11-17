3b0c5848cd7930804ea3bf565297a5f38f4aa797
shayanmalinda <shayanmalinda@gmail.com>
2019-11-17 19:20:54 +0530

    visit driveme activity

---
+++ app/src/main/java/com/driveme/driveme/VisitDrivMe.java
@@ -0,0 +1,14 @@
+package com.driveme.driveme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitDrivMe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_driv_me);
+    }
+}
